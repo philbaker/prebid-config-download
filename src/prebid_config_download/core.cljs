93ee6ab063cc49b094a20f618bc1b5e5a4a8310f
philbaker <phil@philbaker.me>
2023-03-12 20:45:56 +0000

    Ensure commands run in order

--- src/prebid_config_download/core.cljs
+++ src/prebid_config_download/core.cljs
@@ -1,34 +1,43 @@
 (ns prebid-config-download.core
   (:require ["playwright$default" :refer [chromium]]
             ["fs" :as fs]
-            [clojure.test :as t :refer [deftest is async]]
             [borkdude.deflet :refer [defletp defp]]
             [promesa.core :as p]))
 
+(declare browser context page select select-version x download-file)
 (def prebid-version "7.38.0")
-(def prebid-adapters ["appnexus" "ablida"])
+(def prebid-adapters ["adWMG" "medianet" "appnexus" "ablida"])
 
-(defp browser (.launch chromium #js {:headless false}))
-(defp context (.newContext browser))
-(defp page (.newPage context))
-(.goto page "https://docs.prebid.org/download.html")
-(defp select (p/-> (.locator page "select#version_selector")
-                   (.locator "option")
-                   (.allTextContents)))
-(defp select-version (p/-> (.locator page "select#version_selector")
-                           (.selectOption prebid-version)))
+(defn download []
+  (defletp
+    (defp browser (.launch chromium #js {:headless false}))
+    (defp context (.newContext browser))
+    (defp page (.newPage context))
+    (.goto page "https://docs.prebid.org/download.html")
+    (defp select (p/-> (.locator page "select#version_selector")
+                       (.locator "option")
+                       (.allTextContents)))
+    (defp select-version (p/-> (.locator page "select#version_selector")
+                               (.selectOption prebid-version)))
+    (p/loop [x (dec (count prebid-adapters))]
+      (when (> x -1)
+        x
+        (p/recur (- x 1)
+                 (p/-> 
+                   (.locator page (str "input#" (get prebid-adapters x) "BidAdapter"))
+                   (.first)
+                   (.setChecked true)))))
 
-(defp select-bidder
-  (for [adapter prebid-adapters] 
-    (p/-> (.locator page (str "input#" adapter "BidAdapter"))
-          (.first)
-          (.setChecked true))))
+    (do
+      (-> (.waitForEvent page "download")
+          (.then (fn [download] (.path download)))
+          (.then (fn [x] (println (str (fs/readFileSync x))))))
 
-(-> (.waitForEvent page "download")
-    (.then (fn [download] (.path download)))
-    (.then (fn [x] (println (str (fs/readFileSync x))))))
+      (defp download-file
+        (p/-> (.locator page "button.btn.btn-lg.btn-primary")
+              (.first)
+              (.click))))))
 
-(defp download-file
-  (p/-> (.locator page "button.btn.btn-lg.btn-primary")
-        (.first)
-        (.click)))
+(comment
+  (download)
+  )
