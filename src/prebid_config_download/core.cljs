463ad7a156156d8bf1fd0abe7c67acd9dcf38717
philbaker <phil@philbaker.me>
2023-03-13 20:08:36 +0000

    Add log file

--- src/prebid_config_download/core.cljs
+++ src/prebid_config_download/core.cljs
@@ -1,12 +1,22 @@
 (ns prebid-config-download.core
   (:require ["playwright$default" :refer [chromium]]
             ["fs" :as fs]
+            [clojure.string :as str]
             [clojure.edn :refer [read-string]]
             [borkdude.deflet :refer [defletp defp]]
             [promesa.core :as p]))
 
-(def config (read-string (str (fs/readFileSync "config.edn"))))
+(when-not (fs/existsSync "output")
+  (fs/mkdirSync "output"))
 
+(when-not (fs/existsSync "config.edn") 
+  (fs/copyFileSync "config.edn.example" "config.edn"))
+
+(when-not (fs/existsSync "output/log.edn") 
+  (fs/copyFileSync "log.edn.example" "output/log.edn"))
+
+(def config (read-string (str (fs/readFileSync "config.edn"))))
+(def datetime (.toISOString (js/Date.)))
 (declare browser context page select select-version x download-file download-event)
 
 (defn download []
@@ -29,15 +39,39 @@
                      (.locator page ".adapters .checkbox label" #js 
                                {:has (.locator page (str "text=" (get (:prebid-adapters config) x)))})
                      "input")
+                   (.first)
+                   (.setChecked true)))))
+    (p/loop [x (dec (count (:recommended-modules-disable config)))]
+      (when (> x -1)
+        x
+        (p/recur (- x 1)
+                 (p/-> 
+                   (.locator
+                     (.locator page ".checkbox label" #js 
+                               {:has (.locator page (str "text=" (get (:recommended-modules-disable config) x)))})
+                     "input")
+                   (.first)
+                   (.setChecked true)))))
+    (p/loop [x (dec (count (:vendor-specific-modules config)))]
+      (when (> x -1)
+        x
+        (p/recur (- x 1)
+                 (p/-> 
+                   (.locator
+                     (.locator page ".checkbox label" #js 
+                               {:has (.locator page (str "text=" (get (:vendor-specific-modules config) x)))})
+                     "input")
+                   (.first)
                    (.setChecked true)))))
     (p/do 
       (defp download-event
         (p/-> (.waitForEvent page "download")
               (p/then (fn [download] (.path download)))
-              (p/then (fn [x] (fs/copyFileSync x (str (:file-prefix config) (:prebid-version config) ".js"))))
-              (p/then (fn [] (.screenshot page #js {:path "screenshot.png", :fullPage true})))
+              (p/then (fn [x] (fs/copyFileSync x (str "output/" (:file-prefix config) (:prebid-version config) ".js"))))
+              (p/then (fn [] (.screenshot page #js {:path (str "output/" datetime "-prebid-config.png") :fullPage true})))
+              (p/then (fn [] (fs/appendFileSync "output/log.edn" (str (assoc (into {} (dissoc config :headless :file-prefix)) :created-at datetime) "\n"))))
               (p/then (fn [] (.close browser)))
-              (p/then (fn [] (println "download complete")))))
+              (p/then (fn [] (println "download complete - files saved to /output")))))
       (defp download-file
         (p/-> (.locator page "button.btn.btn-lg.btn-primary")
               (.first)
